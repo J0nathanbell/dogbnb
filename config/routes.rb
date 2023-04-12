ca71bc119ff264692cfa9d64ea90974d06cda083
J0nathanbell <jonnybell123@hotmail.com>
2023-04-12 21:48:22 +0100

    inital setup done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'signup', to: 'users#new', as: 'signup'
+  resources :users, only: [:create]
+
+  get 'login', to: 'sessions#new', as: 'login'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
+  root 'pages#index'
+
+  # your other routes
 end
